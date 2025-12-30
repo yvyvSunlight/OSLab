550b77cc05b0bd2d54e081a2a3b8eae44656b94e
LSY <yv.wu@qq.com>
2025-12-30 18:18:18 +0800

    feat: add ret2text Poc

--- command/ret2text.c
+++ command/ret2text.c
@@ -3,7 +3,7 @@ int i;
 int* addr;
 
 void test() {
-    printf("Wuhan University School of Cyber Science and Engineering");
+    printf("backdoor: return to backdoor!\n");
 }
 
 void testat() {
@@ -11,18 +11,13 @@ void testat() {
     for (i = 0; i < 72; i++) {
         buff[i] = 0;
     }
-
-    // for (; i < 72; i++) {
-    //     buff[i] = 0;
-    // }
+    
     addr=&buff[72];
     for (i = 0; i < 4; i++) {
         addr[i] = 0x1000;
     }
-    // printf("asd");
 }
 
 void main(int argc, char* argv[]) {
-    // printf("asd");
     testat();
 }
